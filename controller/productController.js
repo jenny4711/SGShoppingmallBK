7cc0abf1483dfaedf72c4fcbc793383042c9dd8e
jiylee <jjenny4711@gmail.com>
2024-06-03 15:30:15 -0400

    createProduct is fixed

--- controller/productController.js
+++ controller/productController.js
@@ -1,13 +1,15 @@
 const productController={};
 const Product = require('../model/Product');
 
-productController.createProduct=async()=>{
+productController.createProduct=async(req,res)=>{
   try{
-    const {suk,name,size,image,category,description,price,stock,status}=req.body;
-    const product = new Product ({suk,name,size,image,category,description,price,stock,status});
+    console.log(req.body,'createProduct')
+    const {sku,name,size,image,category,description,price,stock,status}=req.body;
+    const product = new Product ({sku,name,size,image,category,description,price,stock,status});
     await product.save();
     return res.status(200).json({status:'createProduct-success',product});
   }catch(error){
+    console.log(error,'createProduct-fail')
     res.status(400).json({status:'createProduct-fail',message:error.message});
   }
 
