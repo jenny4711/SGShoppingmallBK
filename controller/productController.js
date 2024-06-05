d5e7c5f06b13e4acb120159176a9d78759f51748
jiylee <jjenny4711@gmail.com>
2024-06-05 01:01:20 -0400

    abc

--- controller/productController.js
+++ controller/productController.js
@@ -1,6 +1,6 @@
 const productController={};
 const Product = require('../model/Product');
-
+const PAGE_SIZE=1
 productController.createProduct=async(req,res)=>{
   try{
     console.log(req.body,'createProduct')
@@ -17,9 +17,20 @@ productController.createProduct=async(req,res)=>{
 
 productController.getProducts=async(req,res)=>{
   try{
-    const products = await Product.find({});
-    console.log(products,'getProducts')
-    return res.status(200).json({status:'getProducts-success',products});
+    const {page,name}=req.query
+    const cond=name?{name:{$regex:name,$options:'i'}}:{}
+    let query = Product.find(cond)
+    let response ={'status':'success'}
+    if(page){query.skip((page-1)*PAGE_SIZE).limit(PAGE_SIZE)
+     const totalItemNum = await Product.find(cond).count();
+    const totalPageNum =Math.ceil(totalItemNum/PAGE_SIZE);
+    response.totalPageNum=totalPageNum
+    }
+
+   const productList = await query.exec();
+   response.data=productList;
+   console.log(response,'responseProduct')
+    return res.status(200).json(response);
   }catch(error){
     res.status(400).json({status:'getProducts-fail',message:error.message});
   }
